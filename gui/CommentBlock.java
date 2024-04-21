4b9a58d913e3ae8d4676e1414d608f2ade122f12
BLuong-vul <bryanluongrh@gmail.com>
2024-04-21 13:25:31 -0700

    Fixed vote arrows in CommentBlock

--- gui/CommentBlock.java
+++ gui/CommentBlock.java
@@ -38,12 +38,15 @@ public class CommentBlock extends JPanel {
 
         JPanel voteBlock = new JPanel();
         voteBlock.setBackground(Color.WHITE);
-        voteBlock.add(new VoteArrow(Constants.UP));
-
         JLabel voteText = new JLabel("47");
         voteText.setFont(Constants.S_FONT);
+        VoteArrow upvote = new VoteArrow(Constants.UP);
+        VoteArrow downvote = new VoteArrow(Constants.DOWN);
+        upvote.setPair(downvote);
+        
+        voteBlock.add(upvote);
         voteBlock.add(voteText);
-        voteBlock.add(new VoteArrow(Constants.DOWN));
+        voteBlock.add(downvote);
 
         utilityBar.add(voteBlock, BorderLayout.WEST);
         add(utilityBar, gbc);
